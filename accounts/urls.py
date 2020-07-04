66ff142072964ed38d4139b954da276303eeb5e3
Jakub Wolak <jakubwolak.123@gmail.com>
2020-07-04 10:36:51 +0200

    add chaning password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,9 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+from django.urls import path, include
+from django.contrib.auth import views
+from . import views as app_views
 
-app_name = 'users'
+app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
-    path('change_password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
-    path('change_password/done/', auth_views.PasswordChangeDoneView.as_view(), name='change_password_done'),
+    path('', include('django.contrib.auth.urls')),
 ]
