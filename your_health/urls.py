1da104fed2817b626d337424b7f818a3a37cd1c5
Jakub Wolak <jakubwolak.123@gmail.com>
2020-07-04 14:52:09 +0200

    new view with possibility to edit existing user data

--- your_health/urls.py
+++ your_health/urls.py
@@ -7,4 +7,5 @@ app_name = 'your_health'
 
 urlpatterns = [
     path('add_data', views.add_userdata, name='add_data'),
+    path('edit_data', views.edit_userdata, name='edit_data'),
 ]
--- your_health/views.py
+++ your_health/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
+from .decorators import userdata_exists
 from .forms import UserDataForm
+from .models import UserData
 
 
 @login_required
@@ -28,4 +30,29 @@ def add_userdata(request, template_name='your_health/add_userdata.html'):
         'form': form
     }
         
+    return render(request, template_name, context)
+
+
+@login_required
+@userdata_exists
+def edit_userdata(request, template_name='your_health/add_userdata.html'):
+    """
+    edits existing userdata
+    """
+    userdata = get_object_or_404(UserData, user=request.user)
+
+    if request.method == 'POST':
+        form = UserDataForm(request.POST, instance=userdata)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect(reverse('homepage:index'))
+    else:
+        form = UserDataForm(instance=userdata)
+
+    context = {
+        'form': form
+    }
+
     return render(request, template_name, context)
