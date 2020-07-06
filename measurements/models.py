a4af45f27842bbd85854dad511c32777aaeb07e1
Jakub Wolak <jakubwolak.123@gmail.com>
2020-07-06 10:43:24 +0200

    new model for storing user data (single measurement)

--- measurements/models.py
+++ measurements/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from your_health.models import UserData
+
+
+class Measurement(models.Model):
+    userdata = models.ForeignKey(UserData, on_delete=models.PROTECT)
+
+    measurement_time = models.DateTimeField(auto_now_add=True, editable=False)
+
+    systolic_pressure = models.SmallIntegerField(null=False, blank=False, help_text='Ciśnienie skurczowe')
+    diastolic_pressure = models.SmallIntegerField(null=False, blank=False, help_text='Ciśnienie rozkurczowe')
+    pulse = models.SmallIntegerField(null=False, blank=False, help_text='Tętnop')
+
+    def __str__(self):
+        info_message = 'Pomiar dla: {user}, wynik: {systolic}/{diastolic}, tętno: {pulse}'.format(
+            user=userdata.get_full_name(),
+            systolic=self.systolic_pressure,
+            diastolic=self.diastolic_pressure,
+            pulse=self.pulse
+        )
+    
