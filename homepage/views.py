b7307573f7d0b9374d9ce21e9333f812e45c270d
Jakub Wolak <jakubwolak.123@gmail.com>
2020-07-06 14:29:10 +0200

    changes in main page template

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from measurements.mixins import UserDataRequiredMixin
+from measurements.models import Measurement
+from your_health.models import UserData
 
 
-def index(request, template_name='homepage/index.html'):
-    context = {}
+class HomePageView(LoginRequiredMixin, UserDataRequiredMixin, TemplateView):
+    template_name = 'homepage/index.html'
 
-    return render(request, template_name, context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # getting measurements for logged user
+        # mixin provides that this won't throw an exception
+        userdata = UserData.objects.get(user=self.request.user)
+        context['measurements'] = Measurement.objects.filter(userdata=userdata)
+
+        return context
