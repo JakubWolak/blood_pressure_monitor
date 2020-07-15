8a3d964ea2eb51fea8cf3fd7c85ad28820f6c345
Jakub Wolak <jakubwolak.123@gmail.com>
2020-07-15 23:55:55 +0200

    menu view for genetare_files application

--- generate_files/views.py
+++ generate_files/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from your_health.mixins import UserDataRequiredMixin
+
+
+class GenerateFilesMenuView(LoginRequiredMixin, UserDataRequiredMixin, TemplateView):
+    template_name = "generate_files/menu.html"
