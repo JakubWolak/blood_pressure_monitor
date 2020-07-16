9d10a2ca13375cc640368c1d818921d3729e6d6e
Jakub Wolak <jakubwolak.123@gmail.com>
2020-07-16 20:07:09 +0200

    view for generating csv file with measurements

--- generate_files/views.py
+++ generate_files/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import View, TemplateView
 from easy_pdf.views import PDFTemplateView
+import csv
+from django.http import HttpResponse
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from your_health.mixins import UserDataRequiredMixin
@@ -26,5 +28,35 @@ class GeneratePDFView(LoginRequiredMixin, UserDataRequiredMixin, PDFTemplateView
         )
 
 
-class GenerateCSVView(LoginRequiredMixin, UserDataRequiredMixin, TemplateView):
-    template_name = "generate_files/menu.html"
+class GenerateCSVView(LoginRequiredMixin, UserDataRequiredMixin, View):
+    """ generates CSV file for each user """
+
+    def get(self, *args, **kwargs):
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = 'attachment; filename="tabela_pomiarow.csv"'
+
+        writer = csv.writer(response, delimiter="|")
+        writer.writerow(["Tabela pomiarów ciśnienia krwi"])
+        writer.writerow(
+            [
+                "Lp.",
+                "Ciśnienie skurczowe",
+                "Ciśnienie rozkurczowe",
+                "Tętno",
+                "Data pomiaru",
+            ]
+        )
+
+        for id, measurement in enumerate(
+            Measurement.queryset_for_user(self, self.request.user), 1
+        ):
+            row = [
+                id,
+                measurement.systolic_pressure,
+                measurement.diastolic_pressure,
+                measurement.pulse,
+                measurement.measurement_time.strftime("%H:%M  %d %b %Y"),
+            ]
+            writer.writerow(row)
+
+        return response
