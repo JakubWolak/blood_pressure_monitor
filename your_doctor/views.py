19fe6cf0a1dc34572ed2e5f1a990ceada1a5451f
Jakub Wolak <jakubwolak.123@gmail.com>
2020-07-17 21:02:00 +0200

    create and update doctordata forms (not finished yet)

--- your_doctor/views.py
+++ your_doctor/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, FormView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from your_health.mixins import UserDataRequiredMixin
+
+from your_health.models import UserData
+from your_doctor.models import DoctorData
+from your_doctor.forms import DoctorDataForm
+
+
+class DoctorDataCreateView(LoginRequiredMixin, UserDataRequiredMixin, CreateView):
+    template_name = "your_doctor/add_doctordata.html"
+    success_url = reverse_lazy("homepage:index")
+
+    model = DoctorData
+    form_class = DoctorDataForm
+
+    def form_valid(self, form):
+        doctordata = form.save(commit=False)
+        userdata = UserData.objects.get(user=self.request.user)
+        doctordata.userdata = userdata
+
+        messages.add_message(
+            self.requets, messages.INFO, "Pomyślnie zaktualizowano dane"
+        )
+
+        return super(DoctorDataCreateView, self).form_valid(form)
+
+
+class DoctorDataUpdateView(LoginRequiredMixin, UserDataRequiredMixin, UpdateView):
+    model = Doctor
+    form_class = DoctorDataForm
+
+    template_name = "your_doctor/add_data.html"
+    success_url = reverse_lazy("homepage:index")
+
+    def get_initial(self):
+        initial = {}
+
+        try:
+            usserdata = UserData.objects.get(user=self.request.user)
+
+
